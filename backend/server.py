7a44b5bdd0b2bea502fb3be98b49dcf4e6ce92d7
Bardia Barabadi <b.barabadi@gmail.com>
2024-06-27 22:00:45 -0700

    Two way communicate between react and flask achieved

--- backend/server.py
+++ backend/server.py
@@ -1,18 +1,21 @@
 # Filename - server.py
 
 # Import flask and datetime module for showing date and time
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
 import datetime
 
 x = datetime.datetime.now()
 
 # Initializing flask app
 app = Flask(__name__)
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 # Route for seeing a data
 @app.route('/data')
 def get_time():
+	print("Hi world!")
 
 	# Returning an api for showing in reactjs
 	return {
@@ -22,6 +25,15 @@ def get_time():
 		"programming":"python"
 		}
 
+@app.route('/button',  methods=["POST"], strict_slashes=False)
+@cross_origin()
+def get_button():
+	data = request.json
+	print("Button Clicked: " + str(data['data']))
+	# Returning an api for showing in reactjs
+	return {
+		"result": "1"
+    }
 	
 # Running app
 if __name__ == '__main__':
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,47 +3,77 @@
 // Importing modules
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import styled from "styled-components";
+
+const theme = {
+  blue: {
+    default: "#3f51b5",
+    hover: "#283593",
+  }
+}
+
+const Button = styled.button`
+  background-color: ${(props) => theme[props.theme].default};
+  color: white;
+  padding: 5px 15px;
+  border-radius: 5px;
+  outline: 0;
+  border: 0; 
+  text-transform: uppercase;
+  margin: 10px 0px;
+  cursor: pointer;
+  box-shadow: 0px 2px 2px lightgray;
+  transition: ease background-color 250ms;
+  &:hover {
+    background-color: ${(props) => theme[props.theme].hover};
+  }
+  &:disabled {
+    cursor: default;
+    opacity: 0.7;
+  }
+`;
+
+Button.defaultProps = {
+  theme: "blue",
+};
+
+const ButtonToggle = styled(Button)`
+  opacity: 0.7;
+  ${({ active }) =>
+    active &&
+    `
+    opacity: 1; 
+  `}
+`;
+
+function speakerButtonClicked() {
+  alert("You clicked speakerButtonClicked!");
+  fetch(`http://localhost:5000/button`, {
+    'method': 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ "data": "World!" })
+  }).then(res => res.json())
+  .then(function(data) {
+    console.log(data);
+   })
+}
 
 function App() {
-	// usestate for setting a javascript
-	// object for storing and using data
-	const [data, setdata] = useState({
-		name: "",
-		age: 0,
-		date: "",
-		programming: "",
-	});
 
-	// Using useEffect for single rendering
-	useEffect(() => {
-		// Using fetch to fetch the api from 
-		// flask server it will be redirected to proxy
-		fetch("/data").then((res) =>
-			res.json().then((data) => {
-				// Setting a data from api
-				setdata({
-					name: data.Name,
-					age: data.Age,
-					date: data.Date,
-					programming: data.programming,
-				});
-			})
-		);
-	}, []);
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>React and flask</h1>
-				{/* Calling a data from setdata for showing */}
-				<p>{data.name}</p>
-				<p>{data.age}</p>
-				<p>{data.date}</p>
-				<p>{data.programming}</p>
+  return (
+    <div className="App">
+      <header className="App-header">
 
-			</header>
-		</div>
-	);
+        <div>
+          <Button onClick={speakerButtonClicked}>Toggle Speaker</Button>
+        </div>
+
+      </header>
+    </div>
+  );
 }
 
 export default App;
